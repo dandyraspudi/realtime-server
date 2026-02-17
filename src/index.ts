c3847bbd7e0b00493a16699afaaed6ba891b8a4a
Dandy <dandyraspudi@gmail.com>
2026-02-17 20:44:30 +0700

    fix struktur project

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import http from "http";
 import { Server } from "socket.io";
 import cors from "cors";
-import { setupRedisAdapter } from "./lib/redis";
+import { setupRedisAdapter } from "./redis";
 import { matchHistory } from "./lib/history";
 
 const app = express();
